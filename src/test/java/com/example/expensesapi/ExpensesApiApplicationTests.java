254d0675426812b900b1a548f29d1db9ca49eefb
Rodolfo Cunhasque Faedo <faedorodolfo.programming@gmail.com>
2025-11-13 12:40:45 -0300

    first commit

---
+++ src/test/java/com/example/expensesapi/ExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
